17540cca3a5375924fe98cbf82b887a74a3be7e0
hbtalha <helderbatalhadvlpr@gmail.com>
2021-10-07 09:21:30 -0100

    changed theme

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include <QApplication>
 #include <QSettings>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
@@ -10,6 +11,30 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationName("HBatalha");
     QCoreApplication::setApplicationName("MediaCutter");
     QSettings::setDefaultFormat(QSettings::IniFormat);
+
+    QPalette darkPalette;
+    QColor darkColor = QColor(27,27,27);
+    QColor disabledColor = QColor(127,127,127);
+
+    darkPalette.setColor(QPalette::Window, darkColor);
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Base, QColor(0,0,0));
+    darkPalette.setColor(QPalette::AlternateBase, darkColor);
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Disabled, QPalette::Text, disabledColor);
+    darkPalette.setColor(QPalette::Button, darkColor);
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::Disabled, QPalette::ButtonText, disabledColor);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(83,173,203));
+    darkPalette.setColor(QPalette::Highlight, QColor(83,173,203));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+    darkPalette.setColor(QPalette::Disabled, QPalette::HighlightedText, disabledColor);
+
+    qApp->setStyle(QStyleFactory::create("fusion"));
+    qApp->setPalette(darkPalette);
 
     MainWindow w;
     w.show();
